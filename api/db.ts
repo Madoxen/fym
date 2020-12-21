b6f555b8bee4078550be5cb19744357a405672c2
Madoxen <pchlad5@gmail.com>
2020-12-21 18:14:57 +0000

    Added password and login to DB

--- api/db.ts
+++ api/db.ts
@@ -3,10 +3,13 @@ import MongoClient from 'mongodb'
 
 var db: MongoClient.Db;
 
+const db_login = process.env.MONGO_INITDB_ROOT_USERNAME;
+const db_password = process.env.MONGO_INITDB_ROOT_PASSWORD;
+
 
 async function getDb() {
     if (db === undefined)
-        db = (await MongoClient.connect('mongodb://mongodb:27017')).db('fymate')
+        db = (await MongoClient.connect("mongodb://" + db_login + ":" + db_password + "mongodb:27017")).db('fymate')
     return db;
 }
 
