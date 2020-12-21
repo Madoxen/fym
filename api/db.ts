87d8231bc979d1aa3adc1c9785cdf73da6e2ff3b
Madoxen <pchlad5@gmail.com>
2020-12-21 19:25:06 +0000

    Changed env variables name

--- api/db.ts
+++ api/db.ts
@@ -3,13 +3,13 @@ import MongoClient from 'mongodb'
 
 var db: MongoClient.Db;
 
-const db_login = process.env.MONGO_INITDB_ROOT_USERNAME;
-const db_password = process.env.MONGO_INITDB_ROOT_PASSWORD;
+const db_login = process.env.MONGODB_USERNAME;
+const db_password = process.env.MONGODB_PASSWORD;
 
 
 async function getDb() {
     if (db === undefined)
-        db = (await MongoClient.connect("mongodb://" + db_login + ":" + db_password + "mongodb:27017")).db('fymate')
+        db = (await MongoClient.connect("mongodb://" + db_login + ":" + db_password + "@mongo:27017")).db('fymate')
     return db;
 }
 
