2dd545e4b7004a2096cfd38b5099a008a33bfee6
Madoxen <pchlad5@gmail.com>
2020-12-27 20:41:31 +0000

    Added support for custom db url

--- api/db.ts
+++ api/db.ts
@@ -5,11 +5,11 @@ var db: MongoClient.Db;
 
 const db_login = process.env.MONGODB_USERNAME;
 const db_password = process.env.MONGODB_PASSWORD;
-
+const db_location = process.env.MONGODB_URL;
 
 async function getDb() {
     if (db === undefined)
-        db = (await MongoClient.connect("mongodb://" + db_login + ":" + db_password + "@mongo:27017")).db('fymate')
+        db = (await MongoClient.connect("mongodb://" + db_login + ":" + db_password + "@" + db_location)).db('fymate')
     return db;
 }
 
