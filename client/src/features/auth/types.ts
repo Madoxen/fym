3898c4a0baa0dd08121ef9479e93e9bc3f491198
Paweł Chłąd <pchlad5@gmail.com>
2021-01-12 09:46:44 +0000

    Silent refresh (#26)
    * Silent refresher not working
    
    * Finished working on silentRefresh

--- client/src/features/auth/types.ts
+++ client/src/features/auth/types.ts
@@ -1,13 +1,18 @@
-export interface AuthTokensState {
+export interface AuthTokens {
   accessToken: string
   refreshToken: string
+}
+
+export interface AuthTokensState extends AuthTokens {
+  refreshTimeout: number, // Time to token refresh
+  accessTimeout: number, // Time to token refresh
 }
 
 export const REPLACE_TOKENS = 'REPLACE_TOKENS'
 
 interface ReplaceTokensAction {
   type: typeof REPLACE_TOKENS
-  tokens: AuthTokensState
+  tokens: AuthTokens
 }
 
 // Compound type for exporting multiple types under one alias
--- client/src/store.ts
+++ client/src/store.ts
@@ -2,6 +2,7 @@ import { combineReducers, createStore } from 'redux'
 import { devToolsEnhancer } from 'redux-devtools-extension'
 import { CounterReducer } from './features/counter'
 import TokenReducer from './features/auth/reducers'
+import { REPLACE_TOKENS } from './features/auth/types'
 import UsernameReducer from './features/login/loginReducer'
 
 /* Create root reducer, containing all features of the application */
@@ -15,5 +16,45 @@ const store = createStore(
   rootReducer,
   /* preloadedState, */ devToolsEnhancer({})
 )
+
+
+let oldTokens: any = undefined;
+let timeoutID: NodeJS.Timeout;
+store.subscribe(() => {
+  const state = store.getState();
+  if (oldTokens !== state.authTokens) {
+    oldTokens = state.authTokens;
+
+    if (timeoutID !== undefined && timeoutID !== null)
+      clearTimeout(timeoutID);
+
+
+    setTimeout(() => fetch(process.env.REACT_APP_API_URL + '/auth/refresh', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-type': 'application/json; charset=UTF-8',
+        Authorization: "Bearer " + state.authTokens.refreshToken
+      },
+    })
+      .then((r) => r.json())
+      .then((r) =>
+        store.dispatch({
+          type: REPLACE_TOKENS,
+          tokens: { accessToken: r.acc, refreshToken: r.ref },
+        })
+      ).catch(() => {
+
+        store.dispatch({
+          type: REPLACE_TOKENS,
+          tokens: { accessToken: "", refreshToken: "", accessTimeout: Infinity, refreshTimeout: Infinity },
+        })
+
+      }), state.authTokens.accessTimeout * 1000 - Date.now());
+  }
+})
+
+
+
 
 export default store
